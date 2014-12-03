5a4f795b69daaa8a5972ec027f128beb5bd3d027
Vinicius Carvalho <vinixnan@gmail.com.br>
2014-12-03 15:11:13 -0200

    SH files modificated

--- command_to_run_2obj.sh
+++ command_to_run_2obj.sh
@@ -4,8 +4,8 @@ echo "MECBA - Combined 2 objectives"
 
 problem=$1
 
-java -Xms2048m -Xmx5048m -classpath dist/MECBA-Clu-HH.jar jmetal.experiments.Combined_NSGAII_2obj $problem > resultado/time_Experiment_NSGAII_2obj-$problem.txt &
-java -Xms2048m -Xmx5048m -classpath dist/MECBA-Clu-HH.jar jmetal.experiments.Combined_IBEA_2obj $problem > resultado/time_Experiment_IBEA_2obj-$problem.txt &
-java -Xms2048m -Xmx5048m -classpath dist/MECBA-Clu-HH.jar jmetal.experiments.Combined_SPEA2_2obj $problem > resultado/time_Experiment_SPEA2_2obj-$problem.txt &
-#java -Xms2048m -Xmx5048m -classpath dist/MECBA-Clu-HH.jar jmetal.experiments.Experiment_HH_2obj $problem > resultado/time_Experiment_HH_2obj-$problem.txt &
+#java -Xms2048m -Xmx5048m -classpath dist/MECBA-Clu-HH.jar jmetal.experiments.Combined_NSGAII_2obj $problem > resultado/time_Experiment_NSGAII_2obj-$problem.txt &
+#java -Xms2048m -Xmx5048m -classpath dist/MECBA-Clu-HH.jar jmetal.experiments.Combined_IBEA_2obj $problem > resultado/time_Experiment_IBEA_2obj-$problem.txt &
+#java -Xms2048m -Xmx5048m -classpath dist/MECBA-Clu-HH.jar jmetal.experiments.Combined_SPEA2_2obj $problem > resultado/time_Experiment_SPEA2_2obj-$problem.txt &
+java -Xms2048m -Xmx5048m -classpath dist/MECBA-Clu-HH.jar jmetal.experiments.Experiment_HH_2obj $problem > resultado/time_Experiment_HH_2obj-$problem.txt &
 
--- command_to_run_4obj.sh
+++ command_to_run_4obj.sh
@@ -3,7 +3,7 @@ echo "MECBA - Combined 4 objectives"
 
 problem=$1
 
-java -Xms2048m -Xmx5048m -classpath dist/MECBA-Clu-HH.jar jmetal.experiments.Combined_NSGAII_4obj $problem > resultado/time_Experiment_NSGAII_4obj-$problem.txt &
-java -Xms2048m -Xmx5048m -classpath dist/MECBA-Clu-HH.jar jmetal.experiments.Combined_IBEA_4obj $problem > resultado/time_Experiment_IBEA_4obj-$problem.txt &
-java -Xms2048m -Xmx5048m -classpath dist/MECBA-Clu-HH.jar jmetal.experiments.Combined_SPEA2_4obj $problem > resultado/time_Experiment_SPEA2_4obj-$problem.txt &
-#java -Xms2048m -Xmx5048m -classpath dist/MECBA-Clu-HH.jar jmetal.experiments.Experiment_HH_4obj $problem > resultado/time_Experiment_HH_4obj-$problem.txt &
+#java -Xms2048m -Xmx5048m -classpath dist/MECBA-Clu-HH.jar jmetal.experiments.Combined_NSGAII_4obj $problem > resultado/time_Experiment_NSGAII_4obj-$problem.txt &
+#java -Xms2048m -Xmx5048m -classpath dist/MECBA-Clu-HH.jar jmetal.experiments.Combined_IBEA_4obj $problem > resultado/time_Experiment_IBEA_4obj-$problem.txt &
+#java -Xms2048m -Xmx5048m -classpath dist/MECBA-Clu-HH.jar jmetal.experiments.Combined_SPEA2_4obj $problem > resultado/time_Experiment_SPEA2_4obj-$problem.txt &
+java -Xms2048m -Xmx5048m -classpath dist/MECBA-Clu-HH.jar jmetal.experiments.Experiment_HH_4obj $problem > resultado/time_Experiment_HH_4obj-$problem.txt &
