0b1359906b6285363f1ceea5629e6418e4c538b2
Vinicius <vinicius@revendamais.com.br>
2014-12-02 23:28:12 -0200

    HH added

--- command_to_run.sh
+++ command_to_run.sh
@@ -1,11 +1,11 @@
 #!/bin/bash  
 
-#echo "MECBA-Clu - Combined 2 objectives"
-java -Xms2048m -classpath dist/MECBA-Clu.jar jmetal.experiments.Experiment_NSGAII_2obj > resultado/time_Experiment_NSGAII_2obj.txt &
-java -Xms2048m -classpath dist/MECBA-Clu.jar jmetal.experiments.Experiment_PAES_2obj > resultado/time_Experiment_PAES_2obj.txt &
-java -Xms2048m -classpath dist/MECBA-Clu.jar jmetal.experiments.Experiment_SPEA2_2obj > resultado/time_Experiment_SPEA2_2obj.txt &
+#echo "MECBA-Clu-HH - Combined 2 objectives"
+java -Xms2048m -classpath dist/MECBA-Clu-HH.jar jmetal.experiments.Experiment_NSGAII_2obj > resultado/time_Experiment_NSGAII_2obj.txt &
+java -Xms2048m -classpath dist/MECBA-Clu-HH.jar jmetal.experiments.Experiment_PAES_2obj > resultado/time_Experiment_PAES_2obj.txt &
+java -Xms2048m -classpath dist/MECBA-Clu-HH.jar jmetal.experiments.Experiment_SPEA2_2obj > resultado/time_Experiment_SPEA2_2obj.txt &
 
-echo "MECBA-Clu - Combined 4 objectives"
-java -Xms2048m -classpath dist/MECBA-Clu.jar jmetal.experiments.Experiment_NSGAII_4obj > resultado/time_Experiment_NSGAII_4obj.txt &
-java -Xms2048m -classpath dist/MECBA-Clu.jar jmetal.experiments.Experiment_PAES_4obj > resultado/time_Experiment_PAES_4obj.txt &
-java -Xms2048m -classpath dist/MECBA-Clu.jar jmetal.experiments.Experiment_SPEA2_4obj > resultado/time_Experiment_SPEA2_4obj.txt &
+echo "MECBA-Clu-HH - Combined 4 objectives"
+java -Xms2048m -classpath dist/MECBA-Clu-HH.jar jmetal.experiments.Experiment_NSGAII_4obj > resultado/time_Experiment_NSGAII_4obj.txt &
+java -Xms2048m -classpath dist/MECBA-Clu-HH.jar jmetal.experiments.Experiment_PAES_4obj > resultado/time_Experiment_PAES_4obj.txt &
+java -Xms2048m -classpath dist/MECBA-Clu-HH.jar jmetal.experiments.Experiment_SPEA2_4obj > resultado/time_Experiment_SPEA2_4obj.txt &
