4935cf8c40801a0221adb3e3cb915c94f0eef491
Vinicius Carvalho <vinixnan@gmail.com.br>
2014-12-02 23:33:42 -0200

    sh edited

--- command_to_run.sh
+++ command_to_run.sh
@@ -2,10 +2,10 @@
 
 #echo "MECBA-Clu-HH - Combined 2 objectives"
 java -Xms2048m -classpath dist/MECBA-Clu-HH.jar jmetal.experiments.Experiment_NSGAII_2obj > resultado/time_Experiment_NSGAII_2obj.txt &
-java -Xms2048m -classpath dist/MECBA-Clu-HH.jar jmetal.experiments.Experiment_PAES_2obj > resultado/time_Experiment_PAES_2obj.txt &
+java -Xms2048m -classpath dist/MECBA-Clu-HH.jar jmetal.experiments.Experiment_IBEA_2obj > resultado/time_Experiment_IBEA_2obj.txt &
 java -Xms2048m -classpath dist/MECBA-Clu-HH.jar jmetal.experiments.Experiment_SPEA2_2obj > resultado/time_Experiment_SPEA2_2obj.txt &
 
 echo "MECBA-Clu-HH - Combined 4 objectives"
 java -Xms2048m -classpath dist/MECBA-Clu-HH.jar jmetal.experiments.Experiment_NSGAII_4obj > resultado/time_Experiment_NSGAII_4obj.txt &
-java -Xms2048m -classpath dist/MECBA-Clu-HH.jar jmetal.experiments.Experiment_PAES_4obj > resultado/time_Experiment_PAES_4obj.txt &
+java -Xms2048m -classpath dist/MECBA-Clu-HH.jar jmetal.experiments.Experiment_IBEA_4obj > resultado/time_Experiment_IBEA_4obj.txt &
 java -Xms2048m -classpath dist/MECBA-Clu-HH.jar jmetal.experiments.Experiment_SPEA2_4obj > resultado/time_Experiment_SPEA2_4obj.txt &
